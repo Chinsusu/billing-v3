c7b003c7ee22a8294abd6b115a81d0a223ba9aea
Chinsusu <29745944+Chinsusu@users.noreply.github.com>
2026-05-24 09:56:11 +0700

    style: import expire services command

--- apps/backend-laravel/bootstrap/app.php
+++ apps/backend-laravel/bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Console\Commands\ExpireServicesCommand;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -14,7 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withCommands([
-        App\Console\Commands\ExpireServicesCommand::class,
+        ExpireServicesCommand::class,
     ])
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->alias([
